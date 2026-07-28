444f358e6524624037c3e5c676aa596f1394b3ff
pintukhankhan <pintukhankhan405@gmail.com>
2026-07-29 00:36:14 +0530

    Add ECR repository and lifecycle policy resources

--- infra/     terraform/ecr.tf
+++ infra/     terraform/ecr.tf
@@ -1,1 +1,10 @@
-
+resource "aws_ecr_repository" "app" {
+  name = var.project; image_tag_mutability = "MUTABLE"
+  image_scanning_configuration { scan_on_push = true }
+  encryption_configuration { encryption_type = "KMS"; kms_key = aws_kms_key.app.arn }
+}
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+  policy = jsonencode({ rules = [{ rulePriority = 1, selection = { tagStatus = "any", countType = "imageCountMoreThan", countNumber = 20 }, action = { type = "expire" } }] })
+}
+output "ecr_image_uri" { value = "${aws_ecr_repository.app.repository_url}:latest" }
