9ed6cb16621a78dcdac1820c5fba9be196687912
pintukhankhan <pintukhankhan405@gmail.com>
2026-07-28 23:54:52 +0530

    Set environment variables for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,5 @@
-
+import os
+os.environ.setdefault("OPENAI_API_KEY", "sk-test")
+os.environ.setdefault("PINECONE_API_KEY", "pc-test")
+os.environ.setdefault("NEO4J_PASSWORD", "x")
+os.environ.setdefault("SECRET_KEY", "x" * 32)
