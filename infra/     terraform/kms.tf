4dfc447b4182af77de30deb4c1fdb23d720dd571
pintukhankhan <pintukhankhan405@gmail.com>
2026-07-29 00:32:09 +0530

    Add KMS key and alias for app secrets

--- infra/     terraform/kms.tf
+++ infra/     terraform/kms.tf
@@ -1,1 +1,5 @@
-
+resource "aws_kms_key" "app" {
+  description = "${var.project}-${var.env} app secrets"
+  enable_key_rotation = true; deletion_window_in_days = 14
+}
+resource "aws_kms_alias" "app" { name = "alias/${var.project}-${var.env}"; target_key_id = aws_kms_key.app.key_id }
